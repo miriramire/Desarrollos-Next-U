3003c5eea8e99528c30e17e755722bc832185426
miriramire <miriramire@gmail.com>
2017-05-08 17:58:57 -0500

    Create BASE-DECODE-II.js
    function myFunction() {
        var str = "";
        var cont  = 30;
        var mensaje;
        for(var i=0; i<cont;i++){
          var str = reverseString(str);
          var enc = window.atob(str);
          var res = "Encoded String: " + enc;
          mensaje = "The original string: " + str + "<br>" + res + "<br>";
          var str = enc;
        }
        function reverseString(str) {
          return str.split("").reverse().join("");
        }
        alert(mensaje);
    }

---
+++ BASE-DECODE-II.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var str = "";
+    var cont  = 30;
+    var mensaje;
+    for(var i=0; i<cont;i++){
+      var str = reverseString(str);
+      var enc = window.atob(str);
+      var res = "Encoded String: " + enc;
+      mensaje = "The original string: " + str + "<br>" + res + "<br>";
+      var str = enc;
+    }
+    function reverseString(str) {
+      return str.split("").reverse().join("");
+    }
+    alert(mensaje);
+}
