8c8599c09a120d9cd4ace6bef2d45158bd60028c
miriramire <miriramire@gmail.com>
2018-02-28 15:19:06 -0500

    Update express.js

--- JavaScript/Node.JS Parte 2/UNIDAD 2 Implementación TeamApp I/Lección 3 Passport.js III/implementar la modulación MEAN/Modulacion MEAN/server/config/express.js
+++ JavaScript/Node.JS Parte 2/UNIDAD 2 Implementación TeamApp I/Lección 3 Passport.js III/implementar la modulación MEAN/Modulacion MEAN/server/config/express.js
@@ -7,22 +7,23 @@ var passport = require('./passport');
 var session = require('express-session');
 var redisStore = require('connect-redis')(session);
 
+
 module.exports = function(app, config){
-	app.engine('html', swig.renderfile);
+	app.engine('html', swig.renderFile);
 	app.set('view engine', 'html');
 	app.set('views', config.rootPath + '/server/views');
 
 	app.set('view cache', false);
-	swig.setDefaults({cache: false, varControls: ['{^','^}']});
+	swig.setDefaults({ cache: false, varControls: ['{^','^}']});
 
 	app.use(cookieParser());
 	app.use(logger('dev'));
 	app.use(bodyParser());
-
-	app.use(session({store : new redisStore({
-		disableTTL:true
+	
+	app.use(session({ store : new redisStore({
+		disableTTL : true
 	}), secret : 'teamapp next'}));
 	app.use(passport.initialize());
 	app.use(passport.session());
-	app.use(express.static(config.rootPath+'/public'));
+	app.use(express.static(config.rootPath + '/public'));
 };
