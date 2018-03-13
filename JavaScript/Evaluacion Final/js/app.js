e36c99a806c6c1adc5d7afa4a05975a4e271e810
miriramire <miriramire@gmail.com>
2018-03-13 11:30:41 -0500

    Update app.js

--- JavaScript/Evaluacion Final/js/app.js
+++ JavaScript/Evaluacion Final/js/app.js
@@ -1,18 +1,158 @@
-/*Click*/
-var tecla = document.getElementsByClassName("tecla");
+//Variables
+var cero = document.getElementById("0");
+var uno = document.getElementById("1");
+var dos = document.getElementById("2");
+var tres = document.getElementById("3");
+var cuatro = document.getElementById("4");
+var cinco = document.getElementById("5");
+var seis = document.getElementById("6");
+var siete = document.getElementById("7");
+var ocho = document.getElementById("8");
+var nueve = document.getElementById("9");
 
-for (var i = 0; i < tecla.length; i++) {
-  tecla[i].addEventListener("click", function() {
-  	/*alert("funcion");*/
-  	this.style.width = "20%";  	
-    /*var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";*/
-  });
-}
-/*
-document.getElementById("1").addEventListener("click", function(){
-    this.style.width = "20%";
-    this.style.transition = "width 1s"
-    this.style.width = "22%";
-});*/
+var on = document.getElementById("on");
+var masMenos = document.getElementById("sign");
+var raiz = document.getElementById("raiz");
+var divi = document.getElementById("dividido");
+var por = document.getElementById("por");
+var menos = document.getElementById("menos");
+var mas = document.getElementById("mas");
+var igual = document.getElementById("igual");
+var punto = document.getElementById("punto");
+
+//Presion botones
+//uno
+document.getElementById("1").addEventListener("mousedown", function(){
+	uno.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("1").addEventListener("mouseout", function(){
+	uno.setAttribute("style","transform:scale(1,1)")
+});
+//dos
+document.getElementById("2").addEventListener("mousedown", function(){
+	dos.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("2").addEventListener("mouseout", function(){
+	dos.setAttribute("style","transform:scale(1,1)")
+});
+//tres
+document.getElementById("3").addEventListener("mousedown", function(){
+	tres.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("3").addEventListener("mouseout", function(){
+	tres.setAttribute("style","transform:scale(1,1)")
+});
+//cuatro
+document.getElementById("4").addEventListener("mousedown", function(){
+	cuatro.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("4").addEventListener("mouseout", function(){
+	cuatro.setAttribute("style","transform:scale(1,1)")
+});
+//Cinco
+document.getElementById("5").addEventListener("mousedown", function(){
+	cinco.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("5").addEventListener("mouseout", function(){
+	cinco.setAttribute("style","transform:scale(1,1)")
+});
+//Seis
+document.getElementById("6").addEventListener("mousedown", function(){
+	seis.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("6").addEventListener("mouseout", function(){
+	seis.setAttribute("style","transform:scale(1,1)")
+});
+//siete
+document.getElementById("7").addEventListener("mousedown", function(){
+	siete.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("7").addEventListener("mouseout", function(){
+	siete.setAttribute("style","transform:scale(1,1)")
+});
+//ocho
+document.getElementById("8").addEventListener("mousedown", function(){
+	ocho.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("8").addEventListener("mouseout", function(){
+	ocho.setAttribute("style","transform:scale(1,1)")
+});
+//nueve
+document.getElementById("9").addEventListener("mousedown", function(){
+	nueve.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("9").addEventListener("mouseout", function(){
+	nueve.setAttribute("style","transform:scale(1,1)")
+});
+//cero
+document.getElementById("0").addEventListener("mousedown", function(){
+	cero.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("0").addEventListener("mouseout", function(){
+	cero.setAttribute("style","transform:scale(1,1)")
+});
+
+
+//on
+document.getElementById("on").addEventListener("mousedown", function(){
+	on.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("on").addEventListener("mouseout", function(){
+	on.setAttribute("style","transform:scale(1,1)")
+});
+//masMenos
+document.getElementById("sign").addEventListener("mousedown", function(){
+	masMenos.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("sign").addEventListener("mouseout", function(){
+	masMenos.setAttribute("style","transform:scale(1,1)")
+});
+//raiz
+document.getElementById("raiz").addEventListener("mousedown", function(){
+	raiz.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("raiz").addEventListener("mouseout", function(){
+	raiz.setAttribute("style","transform:scale(1,1)")
+});
+//divi
+document.getElementById("dividido").addEventListener("mousedown", function(){
+	divi.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("dividido").addEventListener("mouseout", function(){
+	divi.setAttribute("style","transform:scale(1,1)")
+});
+//por
+document.getElementById("por").addEventListener("mousedown", function(){
+	por.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("por").addEventListener("mouseout", function(){
+	por.setAttribute("style","transform:scale(1,1)")
+});
+//menos
+document.getElementById("menos").addEventListener("mousedown", function(){
+	menos.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("menos").addEventListener("mouseout", function(){
+	menos.setAttribute("style","transform:scale(1,1)")
+});
+//mas
+document.getElementById("mas").addEventListener("mousedown", function(){
+	mas.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("mas").addEventListener("mouseout", function(){
+	mas.setAttribute("style","transform:scale(1,1)")
+});
+//igual
+document.getElementById("igual").addEventListener("mousedown", function(){
+	igual.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("igual").addEventListener("mouseout", function(){
+	igual.setAttribute("style","transform:scale(1,1)")
+});
+//punto
+document.getElementById("punto").addEventListener("mousedown", function(){
+	punto.setAttribute("style","transform:scale(0.95,0.95)")
+});
+document.getElementById("punto").addEventListener("mouseout", function(){
+	punto.setAttribute("style","transform:scale(1,1)")
+});
