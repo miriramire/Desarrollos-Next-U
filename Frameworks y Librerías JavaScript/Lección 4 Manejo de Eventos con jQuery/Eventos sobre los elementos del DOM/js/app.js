d69d1dc113953a2c9671fbc8feda48f8114dc5e0
miriramire <miriramire@gmail.com>
2018-04-02 15:09:54 -0500

    Add files via upload

---
+++ Frameworks y Librerías JavaScript/Lección 4 Manejo de Eventos con jQuery/Eventos sobre los elementos del DOM/js/app.js
@@ -0,0 +1,25 @@
+$(function(){
+  $("#nombre").focus(function(){
+    $(".escondido").css("display", "block");
+    $("#mensaje").text("En este campo debe escribir tu nombre completo");
+  });
+  $("#nombre").blur(function(){
+    $(".escondido").css("display", "none");
+  });
+
+  $("#elementoSelector").change(function(){
+    $(".escondido").css("display", "block").delay(1000).fadeOut("slow");
+    $("#mensaje").text("Cambiaste la opcion del select");
+  });
+
+  $("#testo").select(function(){
+    $(".escondido").css("display", "block").delay(1000).fadeOut("slow");
+    $("#mensaje").text("Seleccionaste un texto");
+  });
+
+  $("form").submit(function(){
+    $(".escondido").css("display", "block").delay(1000).fadeOut("slow");
+    $("#mensaje").text("Enviaste el formulario");
+    event.preventDefault();
+  })
+});
